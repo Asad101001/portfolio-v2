1190436fa1a9235cea076ee3c5b2138cdc76c12b
Asad <muhammadasadk42@gmail.com>
2026-03-06 12:49:01 +0500

    spotify widget rework + image assets not loading fixed

--- app.js
+++ app.js
@@ -1019,12 +1019,14 @@ document.addEventListener('keydown', function (e) {
       dotEl.style.boxShadow  = isPlaying ? '0 0 8px #1DB954' : 'none';
     }
 
-    var artHTML = t.albumArt
+    // Art: either img or empty placeholder
+    var artInner = t.albumArt
       ? '<img class="spotify-art" src="' + t.albumArt + '" alt="" crossorigin="anonymous" />'
       : '<div class="spotify-art spotify-art--empty"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#1DB954"><path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/></svg></div>';
 
+    // 5-bar soundwave when playing, paused icon when not
     var eqOrPause = isPlaying
-      ? '<div class="spotify-eq"><span></span><span></span><span></span><span></span></div>'
+      ? '<div class="spotify-eq"><span></span><span></span><span></span><span></span><span></span></div>'
       : '<svg class="spotify-paused-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="#1DB954" stroke-width="2.5"><circle cx="12" cy="12" r="10"/><line x1="10" y1="15" x2="10" y2="9"/><line x1="14" y1="15" x2="14" y2="9"/></svg>';
 
     var a = document.createElement('a');
@@ -1033,7 +1035,10 @@ document.addEventListener('keydown', function (e) {
     a.rel = 'noopener noreferrer';
     a.className = 'spotify-track';
     a.innerHTML =
-      artHTML +
+      '<div class="spotify-art-wrap">' +
+        artInner +
+        (isPlaying ? '<div class="spotify-spin-ring"></div>' : '') +
+      '</div>' +
       '<div class="spotify-info">' +
         '<div class="spotify-row">' +
           '<div class="spotify-title">' + t.name + '</div>' +
